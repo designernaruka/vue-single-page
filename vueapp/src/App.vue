af027945f6f135e35e7cca95a4746de423bb2c90
Designer Naruka <designernaruka@gmail.com>
2020-06-10 15:47:43 +0530

    table added static data

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,40 +1,17 @@
 <template>
   <div id="app" class="applicaitonPage">
-    <!-- <Users /> -->
-    <Header />
-    <WhatOffer />
-    <WhatIdea />
-    <WhatPortfolio />
-    <WhatTestimonial />
-    <WhatTechnology />
-    <MeetProfessinals />
-    <Footer />
+    <Users />
   </div>
 </template>
 
 <script>
-// import Users from './components/Users.vue'
-import Header from './components/Header.vue'
-import WhatOffer from './components/WhatOffer.vue'
-import WhatIdea from './components/WhatIdea.vue'
-import WhatPortfolio from './components/WhatPortfolio.vue'
-import WhatTestimonial from './components/WhatTestimonial.vue'
-import WhatTechnology from './components/WhatTechnology.vue'
-import MeetProfessinals from './components/MeetProfessinals.vue'
-import Footer from './components/Footer.vue'
+import Users from './components/Users.vue'
+
 
 export default {
   name: 'App',
   components: {
-    // Users,
-    Header,
-    WhatOffer,
-    WhatIdea,
-    WhatPortfolio,
-    WhatTestimonial,
-    WhatTechnology,
-    MeetProfessinals,
-    Footer
+    Users
   }
 }
 </script>
